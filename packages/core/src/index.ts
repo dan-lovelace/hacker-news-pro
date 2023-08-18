da50c561c2170bdadae4f2b92e31353a83ebf390
dan-lovelace <dlovelace085@gmail.com>
2023-08-17 21:11:06 -0400

    chore: begin global selectors

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,10 +1,5 @@
-export const HNP_CONTENT_ELEMENT_ID = "hnp-content";
-export const HNP_HTML_ELEMENT_CLASS_NAME = "hnp";
-export const HNP_ROOT_ELEMENT_ID = "hnp-root";
-export const HNP_SANDBOX_ELEMENT_ID = "hnp-sandbox";
-export const HNP_STYLE_ELEMENT_ID = "hnp-style";
-
 export * from "./browser";
 export * from "./message";
+export * from "./selectors";
 export * from "./storage";
 export * from "./themes";
--- packages/core/src/selectors.ts
+++ packages/core/src/selectors.ts
@@ -1,1 +1,30 @@
-export const SELECTORS = Object.freeze({});
+export const HNP_CONTENT_ELEMENT_ID = "hnp-content";
+export const HNP_HTML_ELEMENT_CLASS_NAME = "hnp";
+export const HNP_ROOT_ELEMENT_ID = "hnp-root";
+export const HNP_SANDBOX_ELEMENT_ID = "hnp-sandbox";
+export const HNP_STYLE_ELEMENT_ID = "hnp-style";
+
+export const SELECTORS = Object.freeze({
+  contentRoot: () => document.getElementById(HNP_ROOT_ELEMENT_ID),
+  styleRoot: () => document.getElementById(HNP_STYLE_ELEMENT_ID),
+});
+
+export function waitForElement(selector: string) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver(() => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.documentElement, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
