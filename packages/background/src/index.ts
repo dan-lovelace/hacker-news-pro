9625b63f7823f08bdf3b6eca70a2bc610b47c085
dan-lovelace <dlovelace085@gmail.com>
2023-09-06 09:29:41 -0400

    chore(background): always show browser action

--- packages/background/src/index.ts
+++ packages/background/src/index.ts
@@ -7,37 +7,16 @@ import {
 import { DEFAULT_THEME_ID } from "@hnp/core/src/themes/default";
 import { TSelectedTheme } from "@hnp/types";
 
-function main() {
-  const { action, declarativeContent, runtime } = browser;
+browser.runtime.onInstalled.addListener(async () => {
+  // apply default theme if no current theme exists such as in a fresh install
+  const { currentTheme } = await fetchThemeData();
 
-  runtime.onInstalled.addListener(async () => {
-    // apply default theme if no current theme exists such as in a fresh install
-    const { currentTheme } = await fetchThemeData();
+  if (!currentTheme) {
+    const defaultTheme: TSelectedTheme = DEFAULT_THEME_ID;
 
-    if (!currentTheme) {
-      const defaultTheme: TSelectedTheme = DEFAULT_THEME_ID;
-
-      await storageSetByKeys({
-        OPTIONS: DEFAULT_OPTIONS,
-        SELECTED_THEME_ID: defaultTheme,
-      });
-    }
-
-    // enable/disable browser action based on selected tab
-    action.disable();
-    declarativeContent.onPageChanged.removeRules(undefined, () => {
-      declarativeContent.onPageChanged.addRules([
-        {
-          conditions: [
-            new declarativeContent.PageStateMatcher({
-              pageUrl: { hostSuffix: "news.ycombinator.com" },
-            }),
-          ],
-          actions: [new declarativeContent.ShowAction()],
-        },
-      ]);
+    await storageSetByKeys({
+      OPTIONS: DEFAULT_OPTIONS,
+      SELECTED_THEME_ID: defaultTheme,
     });
-  });
-}
-
-main();
+  }
+});
